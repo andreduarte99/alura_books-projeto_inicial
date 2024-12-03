5d3e0869c44b860263c43c9ae33efcc3418bcf45
André Duarte <140335@corp.hrln.br>
2024-12-03 18:03:44 -0300

    na metade da aula 3

---
+++ app/metodoFilter.js
@@ -0,0 +1,9 @@
+const btnFiltrarLivrosDeFront = document.getElementById('btnFiltrarLivrosFront');
+
+btnFiltrarLivrosDeFront.addEventListener('click', filtrarLivrosDeFront);
+
+function filtrarLivrosDeFront() {
+    let livrosFiltrados = livros.filter(livro => livro.categoria == 'front-end');
+    console.table(livrosFiltrados);
+
+}
